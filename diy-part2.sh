2115f6a0fdbfa0f83f27aea83d0c3956cadd971a
maohhgg <maohhgg@gmail.com>
2025-11-18 12:32:42 +0800

    Modify default IP and set root password
    Updated default IP address and set default root password.

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,13 +11,16 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
 # Enable USB power for Cudy TR3000 by default
 sed -i '/modem-power/,/};/{s/gpio-export,output = <1>;/gpio-export,output = <0>;/}' target/linux/mediatek/dts/mt7981b-cudy-tr3000-v1.dtsi
+
+# Set default root password to "debian"
+sed -i 's|root:::0:99999:7:::|root:$1$debian$9Q7/4y7Alv6W7tPLJ6x7s0:0:0:99999:7:::|g' package/base-files/files/etc/shadow
